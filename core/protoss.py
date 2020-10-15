1f60d2e1be7a654041bcebc8afe4037624a39cb5
Abdulwahaab <aarif4@github.com>
2020-10-15 18:02:44 -0400

    added vespene geyer structure
    builds vespene geyser for all races

--- core/protoss.py
+++ core/protoss.py
@@ -11,6 +11,7 @@ class Protoss(sc2.BotAI):
         await self.distribute_workers()
         await self.increase_supply_cap()
         await self.build_worker_units()
+        await self.build_vespene_gas_structure()
 
 
     async def build_worker_units(self):
@@ -32,3 +33,22 @@ class Protoss(sc2.BotAI):
             for townhall in self.structures(townhall_unitid).ready:
                 if self.can_afford(supply_unitid):
                     await self.build(supply_unitid, near=townhall)
+
+
+    async def build_vespene_gas_structure(self):
+        townhall_unitid = sc2.constants.NEXUS
+        vgs_unitid = sc2.constants.ASSIMILATOR
+        min_dist = 15.0
+        
+        for townhall in self.structures(townhall_unitid).ready:
+            vg_geysers = self.vespene_geyser.closer_than(min_dist, townhall)
+            for vg_geyser in vg_geysers:
+                if not self.can_afford(vgs_unitid):
+                    break
+                
+                worker = self.select_build_worker(vg_geyser.position)
+                if worker is None:
+                    break
+                
+                if not self.structures(vgs_unitid).closer_than(1, vg_geyser).exists:
+                    worker.build(vgs_unitid, vg_geyser)
--- core/terran.py
+++ core/terran.py
@@ -9,6 +9,7 @@ class Terran(sc2.BotAI):
         await self.distribute_workers()
         await self.increase_supply_cap()
         await self.build_worker_units()
+        await self.build_vespene_gas_structure()
 
 
     async def build_worker_units(self):
@@ -30,3 +31,22 @@ class Terran(sc2.BotAI):
             for townhall in self.structures(townhall_unitid).ready:
                 if self.can_afford(supply_unitid):
                     await self.build(supply_unitid, near=townhall)
+
+
+    async def build_vespene_gas_structure(self):
+        townhall_unitid = sc2.constants.COMMANDCENTER
+        vgs_unitid = sc2.constants.REFINERY
+        min_dist = 15.0
+        
+        for townhall in self.structures(townhall_unitid).ready:
+            vg_geysers = self.vespene_geyser.closer_than(min_dist, townhall)
+            for vg_geyser in vg_geysers:
+                if not self.can_afford(vgs_unitid):
+                    break
+                
+                worker = self.select_build_worker(vg_geyser.position)
+                if worker is None:
+                    break
+                
+                if not self.structures(vgs_unitid).closer_than(1, vg_geyser).exists:
+                    worker.build(vgs_unitid, vg_geyser)
