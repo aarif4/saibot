09759923f521d142284932aad9d9f9d12a8a789f
Abdulwahaab <aarif4@github.com>
2020-10-15 17:05:07 -0400

    Update main.py
    added means to toggle between testing different races' logic

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from core.zerg import Zerg
+from core.terran import Terran
 from core.protoss import Protoss
+from core.zerg import Zerg
 
-
-
-run_game(maps.get("Abyssal Reef LE"), [
-    Bot(Protoss().get_race(), Protoss()),
-    Computer(Race.Protoss, Difficulty.Easy)
+race = 3
+if race == 1:
+    run_game(maps.get("Abyssal Reef LE"), [
+        Bot(Terran().get_race(), Terran()),
+        Computer(Race.Protoss, Difficulty.Easy)
 ], realtime=True)
+elif race == 2:
+    run_game(maps.get("Abyssal Reef LE"), [
+        Bot(Protoss().get_race(), Protoss()),
+        Computer(Race.Protoss, Difficulty.Easy)
+    ], realtime=True)
+elif race == 3:
+    run_game(maps.get("Abyssal Reef LE"), [
+        Bot(Zerg().get_race(), Zerg()),
+        Computer(Race.Protoss, Difficulty.Easy)
+    ], realtime=True)
