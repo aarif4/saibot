d46164c7ff13aa3d6556c0614386505a6038b89e
Abdulwahaab <aarif4@github.com>
2020-10-15 17:07:21 -0400

    generalization of fcns
    done a lot of restructuring where every class has the same base functions that perform a particular operation in a manner unique to the class itself. The goal is to gradually add functionality to all races in sequence.

--- core/protoss.py
+++ core/protoss.py
@@ -1,5 +1,5 @@
 import sc2
-from sc2.constants import  NEXUS, PROBE, PYLON, ASSIMILATOR
+
 
 class Protoss(sc2.BotAI):
     @staticmethod
@@ -9,46 +9,26 @@ class Protoss(sc2.BotAI):
 
     async def on_step(self, iteration: int):
         await self.distribute_workers()
-        await self.build_workers()
-        await self.build_pylons()
-        await self.build_assimilators()
-        await self.build_nexuses()
-        #await self.rush_at_enemy(iteration)
+        await self.increase_supply_cap()
+        await self.build_worker_units()
 
 
-    async def build_workers(self):
-        for nexus in self.structures(NEXUS):
-            if nexus.is_ready and nexus.is_idle:
-                if self.can_afford(PROBE):
-                    nexus.train(PROBE)
+    async def build_worker_units(self):
+        townhall_unitid = sc2.constants.NEXUS
+        worker_unitid   = sc2.constants.PROBE
+        
+        for townhall in self.structures(townhall_unitid):
+            if townhall.is_ready and townhall.is_idle:
+                if self.can_afford(worker_unitid):
+                    townhall.train(worker_unitid)
 
 
-    async def build_pylons(self):
-        if self.supply_left < 5 and not self.already_pending(PYLON):
-            for nexus in self.structures(NEXUS).ready:
-                if self.can_afford(PYLON):
-                    await self.build(PYLON, near=nexus)
-
-
-    async def build_assimilators(self):
-        for nexus in self.structures(NEXUS).ready:
-            vaspenes = self.vespene_geyser.closer_than(15.0, nexus)
-            for vaspene in vaspenes:
-                if not self.can_afford(ASSIMILATOR):
-                    break
-                worker = self.select_build_worker(vaspene.position)
-                if worker is None:
-                    break
-                if not self.structures(ASSIMILATOR).closer_than(1.0, vaspene).exists:
-                    worker.build(ASSIMILATOR, vaspene)
-    
-    async def build_nexuses(self):
-        if self.structures(NEXUS).amount < 3 and self.can_afford(NEXUS):
-            await self.expand_now()
-
-    
-    async def rush_at_enemy(self, iteration):
-        if iteration == 0:
-            for worker in self.workers:
-                worker.attack(self.enemy_start_locations[0])
-
+    async def increase_supply_cap(self):
+        MIN_SUPPLY_THRESH = 5
+        townhall_unitid = sc2.constants.NEXUS
+        supply_unitid = sc2.constants.PYLON
+        
+        if self.supply_left < MIN_SUPPLY_THRESH and not self.already_pending(supply_unitid):
+            for townhall in self.structures(townhall_unitid).ready:
+                if self.can_afford(supply_unitid):
+                    await self.build(supply_unitid, near=townhall)
--- core/terran.py
+++ core/terran.py
@@ -1,9 +1,9 @@
 import sc2
 
-class Zerg(sc2.BotAI):
+class Terran(sc2.BotAI):
     @staticmethod
     def get_race():
-        return sc2.Race.Zerg
+        return sc2.Race.Terran
     
     async def on_step(self, iteration: int):
         await self.distribute_workers()
@@ -12,22 +12,21 @@ class Zerg(sc2.BotAI):
 
 
     async def build_worker_units(self):
-        townhall_unitid = sc2.constants.HATCHERY
-        worker_unitid   = sc2.constants.DRONE
+        townhall_unitid = sc2.constants.COMMANDCENTER
+        worker_unitid   = sc2.constants.SCV
         
         for townhall in self.structures(townhall_unitid):
-            for target_larva in self.larva.closest_n_units(townhall.position, 1):
+            if townhall.is_ready and townhall.is_idle:
                 if self.can_afford(worker_unitid):
-                    target_larva.train(worker_unitid)
+                    townhall.train(worker_unitid)
 
 
     async def increase_supply_cap(self):
         MIN_SUPPLY_THRESH = 5
-        townhall_unitid = sc2.constants.HATCHERY
-        supply_unitid = sc2.constants.OVERLORD
+        townhall_unitid = sc2.constants.COMMANDCENTER
+        supply_unitid = sc2.constants.SUPPLYDEPOT
         
         if self.supply_left < MIN_SUPPLY_THRESH and not self.already_pending(supply_unitid):
             for townhall in self.structures(townhall_unitid).ready:
-                for target_larva in self.larva.closest_n_units(townhall.position, 1):
-                    if self.can_afford(supply_unitid):
-                        target_larva.train(supply_unitid)
+                if self.can_afford(supply_unitid):
+                    await self.build(supply_unitid, near=townhall)
