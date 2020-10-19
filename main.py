8a8100db84049a1b796b9a41751f7c10321a7422
Abdulwahaab <aarif4@github.com>
2020-10-19 10:55:44 -0400

    added toggles
    added toggles to change rance, difficulty, and realtime mode

--- main.py
+++ main.py
@@ -5,19 +5,21 @@ from core.terran import Terran
 from core.protoss import Protoss
 from core.zerg import Zerg
 
-race = 3
+race = 2
+realtime = False
+difficulty=Difficulty.Hard
 if race == 1:
     run_game(maps.get("Abyssal Reef LE"), [
         Bot(Terran().get_race(), Terran()),
-        Computer(Race.Protoss, Difficulty.Easy)
-], realtime=True)
+        Computer(Race.Protoss, difficulty)
+], realtime=realtime)
 elif race == 2:
     run_game(maps.get("Abyssal Reef LE"), [
         Bot(Protoss().get_race(), Protoss()),
-        Computer(Race.Protoss, Difficulty.Easy)
-    ], realtime=True)
+        Computer(Race.Protoss, difficulty)
+    ], realtime=realtime)
 elif race == 3:
     run_game(maps.get("Abyssal Reef LE"), [
         Bot(Zerg().get_race(), Zerg()),
-        Computer(Race.Protoss, Difficulty.Easy)
-    ], realtime=True)
+        Computer(Race.Protoss, difficulty)
+    ], realtime=realtime)
