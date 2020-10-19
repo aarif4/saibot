e1573516d29de282ca753b8b0e847dcde112e448
Abdulwahaab <aarif4@github.com>
2020-10-19 10:19:02 -0400

    attack units added
    protoss can now attack the enemy, beat the computer in easy-mode (all the time) and medium-mode (sometimes). Going to work on getting it to a point where it can beat the medium-mode and hard-mode before generalizing the behavior across all races

--- core/protoss.py
+++ core/protoss.py
@@ -14,8 +14,11 @@ class Protoss(sc2.BotAI):
         await self.build_worker_units()
         await self.build_vespene_gas_structure()
         await self.build_townhall_structure()
-
+        await self.build_combat_structures()
+        await self.train_combat_units()
+        await self.attack_enemy()
         
+
     async def build_worker_units(self):
         townhall_unitid = sc2.constants.NEXUS
         worker_unitid   = sc2.constants.PROBE
@@ -63,3 +66,49 @@ class Protoss(sc2.BotAI):
         if self.structures(townhall_unitid).amount < MAX_NO_TOWNHALLS and self.can_afford(townhall_unitid) and not self.already_pending(townhall_unitid):
             await self.expand_now()
 
+
+    async def build_combat_structures(self):
+        supply_unitid = sc2.constants.PYLON
+        combat_bldg_unitid = sc2.constants.GATEWAY
+        combat_bldg_addon_unitid = sc2.constants.CYBERNETICSCORE
+
+        if self.structures(supply_unitid).ready.exists:
+            target_supply = self.structures(supply_unitid).ready.random
+            if self.structures(combat_bldg_unitid).ready.exists and not self.structures(combat_bldg_addon_unitid):
+                if self.can_afford(combat_bldg_addon_unitid) and not self.already_pending(combat_bldg_addon_unitid):
+                    await self.build(combat_bldg_addon_unitid, near=target_supply)
+            elif len(self.structures(combat_bldg_unitid)) < 3:
+                if self.can_afford(combat_bldg_unitid) and not self.already_pending(combat_bldg_unitid):
+                    await self.build(combat_bldg_unitid, near=target_supply)
+
+
+    async def train_combat_units(self):
+        combat_bldg_unitid = sc2.constants.GATEWAY
+        combat_bldg_addon_unitid = sc2.constants.CYBERNETICSCORE
+        basic_combat_unit = sc2.constants.STALKER
+        
+        for gw in self.structures(combat_bldg_unitid).ready:
+            if gw.is_idle:
+                if self.can_afford(basic_combat_unit) and self.supply_left > 0:
+                    gw.train(basic_combat_unit)
+
+
+    def find_target(self, state):
+        if len(self.enemy_units) > 0:
+            return random.choice(self.enemy_units)
+        elif len(self.enemy_structures) > 0:
+            return random.choice(self.enemy_structures)
+        else:
+            return self.enemy_start_locations[0] # since always 1
+        
+    async def attack_enemy(self):
+        basic_combat_unit = sc2.constants.STALKER
+
+        if self.units(basic_combat_unit).amount > 15:
+            for s in self.units(basic_combat_unit).idle:
+                s.attack(self.find_target(self.state))
+
+        elif self.units(basic_combat_unit).amount > 3:
+            if len(self.enemy_units) > 0:
+                for s in self.units(basic_combat_unit).idle:
+                    s.attack(random.choice(self.enemy_units))
