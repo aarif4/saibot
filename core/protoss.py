0531e9e791d45072d695a1813458523b2f18e27a
Abdulwahaab <aarif4@github.com>
2020-10-19 23:01:01 -0400

    created simplified map
    made intel() function to hold the bare-bones essential information we as an operator would need to know to understand what's going on. It gives us intel on our structures/units as well as the enemy's structures/units (that are in view).

--- core/protoss.py
+++ core/protoss.py
@@ -13,11 +13,14 @@ class Protoss(sc2.BotAI):
     def __init__(self):
         self.iterations_per_minute = 165
         self.max_workers = 65
+        self.do_something_after = 0
+        self.train_data = []
 
     async def on_step(self, iteration: int):
         self.iteration = iteration
         #print(self.iteration/self.iterations_per_minute)
         await self.scout()
+        await self.intel()
         await self.distribute_workers()
         await self.increase_supply_cap()
         await self.build_worker_units()
@@ -27,6 +30,97 @@ class Protoss(sc2.BotAI):
         await self.train_combat_units()
         await self.attack_enemy()
 
+
+    async def intel(self):
+        game_data = np.zeros((self.game_info.map_size[1], self.game_info.map_size[0],3), np.uint8)
+
+        main_base_names = ["nexus", "commandcenter", "hatchery"]
+        for enemy_building in self.enemy_structures:
+            pos = enemy_building.position
+            if enemy_building.name.lower() not in main_base_names:
+                cv2.circle(game_data, (int(pos[0]), int(pos[1])), 5, (200, 50, 212), -1)
+        for enemy_building in self.enemy_structures:
+            pos = enemy_building.position
+            if enemy_building.name.lower() in main_base_names:
+                cv2.circle(game_data, (int(pos[0]), int(pos[1])), 15, (0, 0, 255), -1)
+
+        for enemy_unit in self.enemy_units:
+
+            if not enemy_unit.is_structure:
+                worker_names = ["probe",
+                                "scv",
+                                "drone"]
+                # if that unit is a PROBE, SCV, or DRONE... it's a worker
+                pos = enemy_unit.position
+                if enemy_unit.name.lower() in worker_names:
+                    cv2.circle(game_data, (int(pos[0]), int(pos[1])), 1, (55, 0, 155), -1)
+                else:
+                    cv2.circle(game_data, (int(pos[0]), int(pos[1])), 3, (50, 0, 215), -1)
+        
+        townhall_unitid = sc2.constants.NEXUS
+        worker_unitid   = sc2.constants.PROBE
+        supply_unitid = sc2.constants.PYLON
+        vgs_unitid = sc2.constants.ASSIMILATOR
+        combat_bldg_unitid = sc2.constants.GATEWAY
+        combat_bldg_addon_unitid = sc2.constants.CYBERNETICSCORE
+        combat_bldg_addon2_unitid = sc2.constants.STARGATE
+        arial_combat_unitid = sc2.constants.VOIDRAY
+        scout_unitid = sc2.constants.OBSERVER
+
+        # UNIT/STRUCT: [SIZE,(BGR COLOR)]
+        draw_dict = {
+            townhall_unitid:            [15, (0, 255, 0),1],
+            supply_unitid:              [3, (20, 235, 0),1],
+            worker_unitid:              [1, (55, 200, 0),0],
+            vgs_unitid:                 [2, (0, 200, 55),1],
+            combat_bldg_unitid:         [3, (200, 100, 0),1],
+            combat_bldg_addon_unitid:   [3, (150, 150, 0),1],
+            combat_bldg_addon2_unitid:  [5, (255, 0, 0),1],
+            arial_combat_unitid:        [3, (255, 100, 0),0],
+            scout_unitid:               [1, (255,255,255),0]
+        }
+        for unit_type in draw_dict:
+            if draw_dict[unit_type][-1]:
+                # it's a struct
+                for unit in self.structures(unit_type).ready:
+                    pos = unit.position
+                    cv2.circle(game_data, (int(pos[0]), int(pos[1])), draw_dict[unit_type][0], draw_dict[unit_type][1],-1)
+            else:
+                for unit in self.units(unit_type).ready:
+                    pos = unit.position
+                    cv2.circle(game_data, (int(pos[0]), int(pos[1])), draw_dict[unit_type][0], draw_dict[unit_type][1],-1)
+
+        line_max = 50
+        mineral_ratio = self.minerals / 1500
+        if mineral_ratio > 1.0:
+            mineral_ratio = 1.0
+
+        vespene_ratio = self.vespene / 1500
+        if vespene_ratio > 1.0:
+            vespene_ratio = 1.0
+
+        population_ratio = self.supply_left / self.supply_cap
+        if population_ratio > 1.0:
+            population_ratio = 1.0
+
+        plausible_supply = self.supply_cap / 200.0
+
+        military_weight = len(self.units(arial_combat_unitid)) / (self.supply_cap-self.supply_left)
+        if military_weight > 1.0:
+            military_weight = 1.0
+
+
+        cv2.line(game_data, (0, 19), (int(line_max*military_weight), 19), (250, 250, 200), 3)  # worker/supply ratio
+        cv2.line(game_data, (0, 15), (int(line_max*plausible_supply), 15), (220, 200, 200), 3)  # plausible supply (supply/200.0)
+        cv2.line(game_data, (0, 11), (int(line_max*population_ratio), 11), (150, 150, 150), 3)  # population ratio (supply_left/supply)
+        cv2.line(game_data, (0, 7), (int(line_max*vespene_ratio), 7), (210, 200, 0), 3)  # gas / 1500
+        cv2.line(game_data, (0, 3), (int(line_max*mineral_ratio), 3), (0, 255, 25), 3)  # minerals minerals/1500
+
+        self.flipped = cv2.flip(game_data,0)
+        resized = cv2.resize(self.flipped, dsize=None, fx=2,fy=2)
+        cv2.imshow('Intel', resized)
+        cv2.waitKey(1)
+    
 
     async def scout(self):
         scout_unitid = sc2.constants.OBSERVER
@@ -161,6 +255,7 @@ class Protoss(sc2.BotAI):
             return self.enemy_start_locations[0] # since always 1
         
     async def attack_enemy(self):
+        townhall_unitid = sc2.constants.NEXUS
         basic_combat_unit = sc2.constants.STALKER
         arial_combat_unit = sc2.constants.VOIDRAY
 
