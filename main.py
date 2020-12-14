d3a147b9754debf33dfc9ccf14d71594d2d1a15e
Abdulwahaab <aarif4@github.com>
2020-12-13 23:56:38 -0500

    imports cfg and returns results
    made changes here to allow for config file parsing determine what kind of game we'll run and returns the game's result and how much actual time it took to run that game.

--- main.py
+++ main.py
@@ -24,14 +24,15 @@
     resources.
 """
 import sys
+import time
 import inspect
-from sc2 import run_game, maps, Race, Difficulty
-from sc2.player import Bot, Computer
+import argparse
+import sc2
 import core
+import utils
 
-MAX_ITER = 1 # maximum number of times to run simulation
 
-def run_simulation():
+def run_simulation(logger, user_data):
     """
     This function simply runs the StarCraft API and simulate a game with
     our bot versus the computer. Depending on whether we're in a dev branch
@@ -45,43 +46,34 @@ def run_simulation():
     Output:
         N/A
     """
-    # enemy parameters
-    enemy_race       = Race.Protoss
-    realtime_flag    = False
-    enemy_difficulty = Difficulty.Easy
+    # enemy parameters TODO: In future, make this flexible enough to handle DNN enemy
 
     # depending on which branch we're running, we may not have all of the
     # races available. So only use races that are available in the following
     # priority:
-    if class_exists('Protoss'):
-        run_game(
-            maps.get("Abyssal Reef LE"), 
+    start_time_sec = int(time.time())
+    try:
+        result = sc2.run_game(
+            sc2.maps.get(user_data.cfg['sim_setup']['map_name']),
             [
-                Bot(core.Protoss().get_race(), core.Protoss()),
-                Computer(enemy_race, enemy_difficulty)
-            ], 
-            realtime=realtime_flag )
-    elif class_exists('Terran'):
-        run_game(
-            maps.get("Abyssal Reef LE"), 
-            [
-                Bot(core.Terran().get_race(), core.Terran()),
-                Computer(enemy_race, enemy_difficulty)
-            ], 
-            realtime=realtime_flag )
-    elif class_exists('Zerg'):
-        run_game(
-            maps.get("Abyssal Reef LE"), 
-            [
-                Bot(core.Zerg().get_race(), core.Zerg()),
-                Computer(enemy_race, enemy_difficulty)
-            ], 
-            realtime=realtime_flag )
-    else:
-        errmsg = "No StarCraft Bots available. "
-        errmsg = errmsg + "Please contact @aarif4 for details"
-        raise Exception(errmsg)
+                sc2.player.Bot(
+                    user_data.cfg['player_bot']['race'],
+                    eval(
+                        'core.%s(user_data,"%s")' %
+                        (user_data.cfg['player_bot']['race'].name, 'player_bot'))),
+                sc2.player.Computer(
+                    user_data.cfg['enemy_bot']['race'],
+                    user_data.cfg['enemy_bot']['computer_difficulty'])
+            ],
+            realtime=user_data.cfg['sim_setup']['run_realtime']
+        )
+    except Exception as exp:
+        logger.fatal('Ran into a big error running this simulation. Exiting!')
+        raise exp
 
+    stop_time_sec = int(time.time())
+    time_elapsed_min = float(stop_time_sec - start_time_sec)/60.0
+    return int(result == sc2.Result.Victory), time_elapsed_min
 
 def class_exists(class_name):
     """
@@ -105,6 +97,42 @@ def class_exists(class_name):
 
 
 if __name__ == "__main__":
-    for i in range(MAX_ITER):
-        print('=== Running Starcraft II Trial #%d (out of %d)' % (i+1, MAX_ITER))
-        run_simulation()
+    # create a logger for this category of functions
+    logger, _ = utils.build_logger('main')
+
+    # parse input arguments (it's required btw)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(\
+        '-c', \
+        dest='config_file', \
+        type=str, \
+        help='path to INI configuration file')
+    args = parser.parse_args()
+
+    # use input arguments to read a configuration file (it's required btw)
+    user_data = utils.config_data(args.config_file)
+    logger.info('Configuration file contents:\n%s', user_data)
+
+    # run the requested number of trials (>0) and report the winrate
+    num_wins = float(0)
+    win_loss = []
+    time_taken_min = []
+    num_trials = user_data.cfg['sim_setup']['num_iterations']
+    for i in range(num_trials):
+        s = 'Running Starcraft II Trial #%d (out of %d)' % (i+1, num_trials)
+        if win_loss:
+            s = '%s [Win Rate: %.2f%%] [Avg. Time Per Trial: %.2f min]' % \
+                    (s, sum(win_loss)/len(win_loss)*100, sum(time_taken_min)/len(time_taken_min))
+
+        logger.info(s)
+        win_loss_val, time_taken_min_val  = run_simulation(logger, user_data)
+        win_loss.append(win_loss_val)
+        time_taken_min.append(time_taken_min_val)
+
+    logger.info("=================================")
+    logger.info("Done Running StarCraft II Trials!")
+    logger.info("=================================")
+    logger.info("Win Rate: %d/%d: %.2f%%", num_wins, num_trials, sum(win_loss)/num_trials*100)
+    logger.info('Total Time Taken for %d Trials:  %.4f hours (%.4f min)', \
+        num_trials, sum(time_taken_min)/24, sum(time_taken_min))
+    logger.info('Avg. Time Taken Per Trial:       %.4f minutes', sum(time_taken_min)/num_trials)
